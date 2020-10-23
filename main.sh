ebfd724c2a30d170703b79626b939eb4a9d261d1
Fatma Güney <fguney@ku.edu.tr>
2020-10-23 08:53:11 +0000

    Solutions

--- main.sh
+++ main.sh
@@ -1,1 +1,25 @@
 echo 'Hello World'
+
+$SHELL
+
+mkdir practicals
+touch --help
+
+touch practicals/semester
+
+cd practicals
+
+# https://en.wikipedia.org/wiki/Shebang_(Unix)
+
+echo '#!/bin/sh' > semester
+echo 'curl --head --silent https://missing.csail.mit.edu' >> semester
+
+sh semester
+
+chmod --help
+
+chmod +x semester
+
+./semester
+
+./semester | grep -i "last-modified" | cut -d ' ' -f 2- > last-modified.txt
